4c8c9c4b1663fff5e244ea6859bd578ada9d48b4
Manish Rayamajhi <37249602+manishry@users.noreply.github.com>
2018-05-30 10:22:10 +0545

    web design

---
+++ project/index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>prject</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  
+	
+   <link rel="stylesheet" type="text/css" href="style.css">
+   
+<style type="text/css">
+ .zero-margin{
+    margin: 0px;
+ }
+ .zero-padding{
+  padding: 0px;
+ }
+ 
+</style>
+  
+
+
+</head>
+<body>
+	<div class="row top-menu">
+		<div class="container">
+  	
+  		<div class="header-top-content">
+  		  			<a href="">Home | </a>
+  		  			<a href="">Aims |</a>
+  		  			<a href="">Our Team |</a>
+  		  			<a href=""> About |</a>
+              <a href="">UsContact |</a>
+  		  			<a href="">UsNewsletter |</a>
+  		  			<a href="">Videos |</a>
+  		  			<a href="">Forums |</a>
+  		  		</div>
+  		  	
+	</div>
+</div>
+
+  
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-primary" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+ <!--main page image slider-->
+<div class="container">
+ <div class="row">
+  <div class="col-xs-12">
+  <div class="col-xs-3">
+    <ul class="list-group">
+      <li class="list-group-item"><h2><b>menu bar</b></h2></li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+      <li class="list-group-item"><a href="#">menu bar</a> </li>
+       
+    </ul>
+  </div>
+<div class="col-xs-9 ">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="image/img5.jpeg" width="100%" height="6%" alt="Los Angeles">
+    </div>
+
+    <div class="item">
+      <img src="image/img4.jpeg" width="100%" height="6%" alt="Chicago">
+    </div>
+
+    <div class="item">
+      <img src="image/img9.jpg" width="100%" height="6%" alt="New York">
+    </div>
+
+    <div class="item">
+      <img src="image/img9.jpg" width="100%" height="6%" alt="New York">
+    </div>
+
+    <div class="item">
+      <img src="image/img9.jpg" width="100%" height="6%" alt="New York">
+    </div>
+
+    <div class="item">
+      <img src="image/img9.jpg" width="100%" height="6%" alt="New York">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="row" style="margin-top: 10px;">
+  <div class="col-md-12 zero-margin zero-padding" >
+ <div class="col-md-6">
+    <a href="#"><img src="image/img9.jpg" width="100%"></a>
+    
+  </div>
+
+  <div class="col-md-6">
+   <a href="#"> <img src="image/img9.jpg" width="100%"></a>
+    
+  </div>
+</div>
+<div class="col-md-12 zero-margin zero-padding" style="margin-top: 10px;">
+  
+   <div class="col-md-6">
+    
+    <a href="#"><img src="image/img9.jpg"  width="100%" ></a>
+  </div>
+
+  <div class="col-md-6">
+   
+    <a href="#"><img src="image/img9.jpg"  width="100%" ></a>
+  </div>
+   </div>
+ </div>
+</div>
+</div>
+
+  
+    
+
+   
+
+
+
+
+<!-- end of slider -->
+<br>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 news-links">
+          <div>
+            <img src="image/img8.jpeg" width="100%">
+          </div>
+         
+         <div class="row" style="margin-top: 20px">
+           <div class="col-md-6" style="padding-bottom: 20px">
+             <img src="image/img4.jpeg" width="100%">
+           </div>
+           <div class="col-md-6" style="padding-bottom: 20px">
+             <img src="image/img2.jpg" width="100%">
+           </div>
+         </div>
+         </div>
+        <div class="col-md-4">
+          <div class="right-post" style="margin-top: 50px">
+             <img src="image/img7.jpeg" width="100%">
+          </div>
+         
+         <div class="right-post" style="margin-top: 40px">
+             <img src="image/img6.jpeg" width="100%">
+          </div>
+
+          <div class="right-post" style="margin-top: 40px">
+             <img src="image/img5.jpeg" width="100%">
+          </div>
+
+          
+
+          
+
+        </div>
+      </div>
+      </div>
+    </div>
+          
+<!--Footer-->
+<footer class="page-footer center-on-small-only stylish-color-dark">
+
+    <!--Footer Links-->
+    <div class="container">
+        <div class="row">
+
+            
+            
+            
+            <!--/.Third column-->
+
+            <hr class="clearfix w-100 d-md-none">
+
+            <!--Fourth column-->
+            <div class="col-md-4 mx-auto">
+                <h2 class="title mb-4 mt-3 font-bold ">Interact</h2>
+                <ul>
+                    <li><a href="#!">Like Us On Facebook</a></li>
+                    <li><a href="#!">Follow Us On Twitter</a></li>
+                    <li><a href="#!">Join Us On Google+</a></li>
+                    <li><a href="#!"> Subscribe RSS Feeds</a></li>
+                     <li><a href="#!"> Contact us</a></li>
+                    
+                </ul>
+            </div>
+            <!--/.Fourth column-->
+        </div>
+         
+<hr>
+<!--Social buttons-->
+    <div class="social-section pull-left">
+        <ul>
+
+            <li><a class="btn-floating btn-sm btn-fb"><i class="fa fa-facebook"> </i></a></li>
+            <li><a class="btn-floating btn-sm btn-tw"><i class="fa fa-twitter"> </i></a></li>
+            <li><a class="btn-floating btn-sm btn-gplus"><i class="fa fa-google-plus"> </i></a></li>
+            <li><a class="btn-floating btn-sm btn-li"><i class="fa fa-linkedin"> </i></a></li>
+            <li><a class="btn-floating btn-sm btn-dribbble"><i class="fa fa-dribbble"> </i></a></li>
+
+        </ul>
+    </div>
+    <!--/.Social buttons-->
+
+    <!--Copyright-->
+    <div class="footer-copyright">
+        <div class="container-fluid">
+            © 2017 Copyright: <a href="https://www.MDBootstrap.com"> MDBootstrap.com </a>
+
+        </div>
+    </div>
+    <!--/.Copyright-->
+
+    </div>
+    <!--/.Footer Links-->
+
+    
+
+</footer>
+<!--/.Footer-->
+        
+
+
+       <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+       <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+        
+</body>
+</html>
+ 
